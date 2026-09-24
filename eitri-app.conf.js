5e73918f71167e93357663398a730ec81205a515
Gabriel Silva <gabriel.silva@calindra.com.br>
2026-09-24 15:24:05 -0300

    feat: update deeplink resolver configuration and restore workflow for version verification

--- eitri-app.conf.js
+++ eitri-app.conf.js
@@ -1,17 +1,18 @@
 module.exports = {
-  "name": "eitri-shopping-addons-deeplink-resolver-deco",
-  "title": "eitri-shopping-addons-deeplink-resolver-deco",
-  "slug": "eitri-shopping-addons-deeplink-resolver-deco",
+  name: "eitri-shopping-addons-deeplink-resolver-deco",
+  title: "eitri-shopping-addons-deeplink-resolver-deco",
+  slug: "eitri-shopping-addons-deeplink-resolver-deco",
   "eitri-luminus": "2.22.6",
   "eitri-bifrost": "6.0.0",
   "eitri-commons": "2.2.1",
-  "version": "0.1.0",
-  "id": "b0119535-e179-4459-aca0-4d49c4aa7661",
+  version: "1.0.0",
+  messageVersion: "feat: new deeplink mapping resolver",
+  id: "b0119535-e179-4459-aca0-4d49c4aa7661",
   "eitri-app-dependencies": {
     "eitri-shopping-vtex-shared": {
-      "version": "1.1.4"
-    }
+      version: "1.1.4",
+    },
   },
-  "type": "module",
-  "check-js": false
-}
+  type: "module",
+  "check-js": false,
+};
